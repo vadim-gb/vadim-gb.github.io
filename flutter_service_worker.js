c013c9ea34d7587941b36cc9ca7dcb7037a7a319
vadim_GB <mrronalgames@yandex.ru>
2023-06-17 19:33:40 +0300

    ну типа рабочии шутки хуютки

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,8 +6,8 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "52fc34a70b225832e47b13c384bc9b3e",
-"assets/NOTICES": "0b200a7540a512cbf29acbf19703131e",
+"assets/fonts/MaterialIcons-Regular.otf": "e7fd832753bbec0a94ebc11efde20718",
+"assets/NOTICES": "38e9e2cf759025cf92e99e116bfe36ba",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "57d849d738900cfd590e9adc7e208250",
 "assets/shaders/ink_sparkle.frag": "f8b80e740d33eb157090be4e995febdf",
 "canvaskit/canvaskit.js": "76f7d822f42397160c5dfc69cbc9b2de",
@@ -23,9 +23,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "de9f27e6f90a11bf3a6696b76835c855",
-"/": "de9f27e6f90a11bf3a6696b76835c855",
-"main.dart.js": "f54e6f4583c99843808c7de7c59cf46a",
+"index.html": "a7240de19314897e4aa474920ab746a6",
+"/": "a7240de19314897e4aa474920ab746a6",
+"main.dart.js": "e7b2ae0f17232a22d757c04b6cbce8c8",
 "manifest.json": "3ba0ba4f978c783f0c6313de404c4e7b",
 "version.json": "c3cfdb70a53009fcc4277fb49f72a57a"};
 // The application shell files that are downloaded before a service worker can
